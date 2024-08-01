99f250619438bf8bb045029a26103bd6429af768
Piyush Mali <piyushmali702@gmail.com>
2024-08-01 15:35:26 +0530

    updated

--- contracts/1_HelloWorld.sol
+++ contracts/1_HelloWorld.sol
@@ -1,8 +1,11 @@
+// Problem Statement
+// Write a contract to print a string "Hello World!". Printing should not involve any gas.
+
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.25;
 
 contract HelloWorld {
-    function print() public pure returns (string memory){
+    function print() public pure returns (string memory) {
         return "Hello World!";
     }
 }
